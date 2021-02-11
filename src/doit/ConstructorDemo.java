b597b96dd680faa52f4e980cc7de4505ad00c066
jgam <jgam@alumni.nd.edu>
2021-02-11 21:37:44 +0900

    done

---
+++ src/doit/ConstructorDemo.java
@@ -0,0 +1,11 @@
+package doit;
+
+public class ConstructorDemo {
+    public ConstructorDemo(int param1){
+
+    }
+
+    public static void main(String[] args){
+        ConstructorDemo c = new ConstructorDemo(3);
+    }
+}
